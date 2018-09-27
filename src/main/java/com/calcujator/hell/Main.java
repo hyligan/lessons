4cbc4f7798083eac7b8fe0d2be4168a34582af9b
krasava <hyligan.e@gmail.com>
2018-09-27 18:58:11 +0300

    start from maria

--- src/main/java/com/calcujator/hell/Main.java
+++ src/main/java/com/calcujator/hell/Main.java
@@ -11,9 +11,9 @@ public class Main {
         Display display = new Display();
         Keyboard keyboard = new Keyboard();
 
-        display.show ("Hello world from display");
-        double a = keyboard.vvod("Введите первое число: ", display);
-        keyboard.vvod(text, display,a, keyboard);
+        display.show ("Hello world frodfsfsdfm display");
+        double c = keyboard.vvod("Введите первое число: ", display);
+        keyboard.vvod(text, display,c, keyboard);
         double b = keyboard.vvod("Введите второе число: ", display);
     }
 
